6457aa1ddadbd244d177b7f903c32ddcb23c448c
Spoderman <ethan.duggan@uoit.net>
2019-03-06 22:15:03 -0500

    finished Question 2 such that it functions properly.  Might further modify so that it appears more like the image in the Assignment pdf if I have time, not sure if necessary

--- Q2/src/Main.java
+++ Q2/src/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import java.lang.Math;
 
 public class Main extends Application {
     public static void main(String[] args){
@@ -30,12 +31,14 @@ public class Main extends Application {
 
         Button calculateButton = new Button("Calculate");
 
-        //calculateButton.setOnAction((e) -> {futureValueField.setText();});
+        calculateButton.setOnAction((e) -> {double investmentAmount = Double.parseDouble(investmentAmountField.getText());
+                                            int months = Integer.parseInt(yearsField.getText()) * 12;
+                                            double monthlyInterestRate = Double.parseDouble(interestRateField.getText()) / 1200; //1200 comes from needing to divide by 100 to convert from percentage plus needing to divide by 12 to convert to monthly
+                                            double futureValue = investmentAmount * Math.pow((1 + monthlyInterestRate), months);
+                                            futureValueField.setText(Double.toString(futureValue));});
 
         pane.addColumn(0, investmentAmountLabel, yearsLabel, interestRateLabel, futureValueLabel);
         pane.addColumn(1, investmentAmountField, yearsField, interestRateField, futureValueField, calculateButton);
-
-
 
         stage.setScene(scene);
         stage.show();
